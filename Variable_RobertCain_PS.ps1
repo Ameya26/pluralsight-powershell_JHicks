7bb847eed04d357b3a39d77e8a32157ece8b0a5d
Ameya <deshpande.ameya@hotmail.com>
2017-09-12 16:44:10 -0400

    Added few more powershell commands

--- Variable_RobertCain_PS.ps1
+++ Variable_RobertCain_PS.ps1
@@ -107,6 +107,167 @@ dasd Blank liens
 
 $heretext
 
+# also works with single quote string
+
+
+# String interpolation
+Clear-Host
+$items = (Get-ChildItem).Count
+$loc = Get-Location
+
+"There are $items items in the folder $loc"
+
+# To actually display the vairable, escape it with a backtick
+"There are `$items items are in the folder `$loc"
+
+# string interpolation works with only double quotes
+
+# Can use expression in string, need to be wrapped in $()
+Clear-Host
+
+"There are $((Get-ChildItem).Count) items are in the folder $(Get-Location)"
+
+# String formating - C# like syntax is supported
+[String]::Format("There are {0} items. ", $items)
+
+# Powershell shortcuts
+"There are {0} items ...." -f $items
+
+"There are {0} items in the location {1} " -f $items, $loc
+
+# Additional formating can be provided using {0:0.00}
+"THerea are {0:0.00} dollar in my account " -f (33.13 * 323)
+
+# Wildcards
+Clear-Host
+"Pluralshight" -like "Plural*"
+"Pluralsight" -like "arcane*"
+"Pluralsight" -like "?luralsight"    # question mark work for single characters
+"Pluralsight" -like "Plural*[s-v]"   # ends in a char btw s and v
+"Pluralsight" -like "Plural*[a-c]"   # ends in a char btw a and c
+
+# Regular expressions
+Clear-Host
+"888-011-3125" -match "[0-9]{3}-[0-9]{3}-[0-9]{4}" # phone validation
+"100.122.0122" -match "[0-9]{3}-[0-9]{3}-[0-9]{4}"
+
+#---------------------------------------------------------------#
+# Array
+#---------------------------------------------------------------#
+
+Clear-Host
+$array = "ameya", "deshpande"
+$array[0]
+$array[1]
+$array
+
+$array.GetType()
+
+#updating arrays
+$array = "plural", "sight"
+$array
+
+# Formal array creation syntax
+$array = @("mayura", "deshpande")
+$array
+
+# Empty array can be created only formal array creation syntax
+$array = @()
+$array.Count
+
+# Cool method to load arrays using numeric range notation
+$array = 1..5
+$array
+
+# Item exits in an array
+Clear-Host
+$array = @(1, 43,323)
+$array -contains 43
+$array -inotcontains 99
+$array -notcontains 43
+
+#---------------------------------------------------------------#
+# Hash tables
+#---------------------------------------------------------------#
+
+Clear-Host
+
+$hash = @{"Key" = "Value";
+          "Pluralsight" = "pluralsight.com";}
+
+$hash                # Display all values
+$hash["Pluralsight"] # Get a single value from the key
+
+$hash."Pluralsight"  # Get single value using object syntax
+
+# Can be used as a variable as keys
+$mykey = "Pluralsight"
+$hash.$mykey       # using variable as property
+$hash.$($mykey)    # evaluating as an expression
+$hash.$("Plural" + "Sight")
+
+# Adding and removing
+$hash
+
+$hash["new Key"] = "top gera.com"
+$hash.Remove("Key")
+$hash
+
+# see if key exists
+$hash.Contains("new Key")
+$hash.Contains("key")
+
+# See if value exists using ContainsValue
+$hash.ContainsValue("value")
+$hash.ContainsValue("top gera.com")
+
+# list keys and values
+$hash.Keys
+$hash.Values
+
+# Find if key or value is present in the hash table
+$hash.Keys -contains "new key"
+
+$hash.Values -contains "top gera.com"
+
+
+#---------------------------------------------------------------#
+# Built in variables
+#---------------------------------------------------------------#
+
+Clear-Host
+
+# False and True
+$false
+$true
+
+# Null
+$Null
+
+# current directory $pwd
+$PWD
+
+# Users home directory $Home
+$HOME
+
+# Info about a user machine $Host
+$Host
+
+# Process ID
+$PID
+
+# Current version of the powershell $PSVersionTable
+$PSVersionTable
+
+# $_ # Current Object
+Get-ChildItem | Where-Object {$_.Name -like "*.ps1"}
+Get-ChildItem | Where-Object {$_.Name -like "*.txt"}
+
+
+
+
+
+
 
 
 
