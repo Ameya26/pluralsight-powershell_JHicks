3a151eece98bb3178074c9446bfd4ce2bd960007
Ameya <deshpande.ameya@hotmail.com>
2017-09-13 19:50:50 -0400

    Added files help

--- Programming_RobertCain_PS.ps1
+++ Programming_RobertCain_PS.ps1
@@ -432,8 +432,6 @@ Clear-Host
 &{ $localvar = 43; Write-Host "Inside the block $localvar" }
 Write-Host "Outside the block $localvar"
 
-
-
 #---------------------------------------------------------------#
 # Programming - Functions
 #---------------------------------------------------------------#
@@ -463,6 +461,374 @@ Set-FVar([ref]$fvar) # must add ref to call
 "favr after a $fvar"
 
 # read from pipeline
+function Get-CoolFiles()
+{
+    begin {$retval = "Here are some cool files: `r`n"}
+    process {
+        if($_.Name -like "*.ps1")
+        {
+            $retval = $retval + "`t " + $_.Name + "`r`n"
+        }
+    }
+    end {return $retval}
+}
+
+Set-Location "C:\GitRepos\PluralSight\Powershell\pluralsight-powershell_JHicks"
+
+Clear-Host
+Get-ChildItem | Get-CoolFiles
+
+# Above works fine but ties it to PS1
+
+# instead use filter function, Filters can be built to remove unwanted files
+filter Show-PS1Files
+{
+    $filename = $_.Name
+    if($filename -like "*.ps1")
+    {
+        return $_
+    
+    }
+}
+
+Clear-Host
+
+Get-ChildItem | Show-PS1Files
+
+# This version doesn't check for the ps1, it just lists what's passed in
+function List-FileNames
+{
+    begin {$retval = "This will list all the files in the current directory `r`n"}
+    process {$retval = $retval + "`t" + $_.Name + "`r`n"}
+    end { return $retval }
+}
+
+Clear-Host
+Get-ChildItem | List-FileNames
+
+# Now combine the two for real flexibility
+Get-ChildItem | Show-PS1Files | List-FileNames
+
+# Now to do other files txt files, just create another filter
+filter Show-TxtFiles
+{
+    $filename = $_.Name
+    if ($filename -like "*.txt")
+    {
+        return $_
+    }
+}
+
+Get-ChildItem | Show-TxtFiles | List-FileNames
+
+# Now you have two ways to us your function
+Clear-Host
+Get-ChildItem | Show-PS1Files | List-FileNames
+Get-ChildItem | Show-TxtFiles | List-FileNames
+
+
+# having your function output to the pipeline
+Clear-Host
+Set-Location "C:\GitRepos\PluralSight\Powershell\pluralsight-powershell_JHicks"
+Get-ChildItem | Select-Object "Name"
+
+function Get-ChildName()
+{
+    Write-Output (Get-ChildItem | Select-Object "Name")
+}
+
+Clear-Host
+
+Get-ChildName | Where-Object {$_.Name -like "*.txt"}
+
+# Supporting -verbose and -debug options
+# To support, first need to change the values of the special variables:
+# $DebugPreference   Default is SilentlyContinue, change to Continue
+# $VerbosePreference Default is SilentlyContinue, change to COntinue
+
+function Get-ChildName()
+{
+    param([switch]$verbose, [switch]$debug)
+
+    if($verbose.IsPresent)
+    {
+        $VerbosePreference = "Continue"
+    }
+    else
+    {
+        $VerbosePreference = "SilentlyContinue"
+    }
+    if($debug.IsPresent)
+    {
+        $DebugPreference = "Continue"
+    }
+    else
+    {
+        $DebugPreference = "SilentlyContinue"
+    }
+
+    Write-Verbose "Currently working location is $(Get-Location)"
+    Write-Output (Get-ChildItem | Select-Object "Name")
+    Write-Debug "Ok I have selected some...."
+}
+
+Clear-Host
+
+Get-ChildName
+Get-ChildName -verbose
+Get-ChildName -debug
+Get-ChildName -verbose -debug
+
+#---------------------------------------------------------------#
+# Adding help to YOur functions
+#---------------------------------------------------------------#
+Clear-Host
+
+Get-Help  Get-ChildName
+
+# Custom tags within a comment block that Get-Help will recognize
+# Note that not all of them are required
+# .SYNOPSIS - A brief description of the Command
+# .DESCRIPTION - Detailed command description
+# .PARAMETER Name - Include one description for each parameter
+# .EXAMPLE - Detailed examples on how to use the command
+# .INPUTS - what pipeline inputs are supported
+# .OUTPUTS - What this function outputs
+# .NOTES - Any misc notes you havent put anywhere else
+# .LINK - A link to the URL for more help, Use one .LINK per URL
+# Use "Get-Help about_commen_based_help" for full list and details
+
+
+
+function Get-ChildName()
+{
+<#
+    .SYNOPSIS
+    Returns a list of only the names for the child items in the current directory
+
+    .DESCRIPTION
+    This function is similar to Get-ChildItem, except that it returns only the name property
+#>
+
+    param([switch]$verbose, [switch]$debug)
+
+    if($verbose.IsPresent)
+    {
+        $VerbosePreference = "Continue"
+    }
+    else
+    {
+        $VerbosePreference = "SilentlyContinue"
+    }
+    if($debug.IsPresent)
+    {
+        $DebugPreference = "Continue"
+    }
+    else
+    {
+        $DebugPreference = "SilentlyContinue"
+    }
+
+    Write-Verbose "Currently working location is $(Get-Location)"
+    Write-Output (Get-ChildItem | Select-Object "Name")
+    Write-Debug "Ok I have selected some...."
+}
+
+
+Clear-Host
+
+Get-Help Get-ChildName -Full
+
+
+#---------------------------------------------------------------#
+# Error handling
+#---------------------------------------------------------------#
+
+function Divver($enum, $denom)
+{
+    Write-Host "Divver Begins"
+    $result = $enum / $denom
+    Write-Host "Result = $result"
+    Write-Host "Divver done "
+
+    trap{
+        Write-Host "Oh No!!! An error has occurred !!!"
+        Write-Host $_.ErrorID
+        Write-Host $_.Exception.Message
+        continue # continue will continue with the next line of the code after the error
+    }
+
+}
+
+Clear-Host
+Divver 22 11
+
+Divver 33 0
+
+# break in trap
+
+function Divver($enum, $denom)
+{
+    Write-Host "Divver Begins"
+    $result = $enum / $denom
+    Write-Host "Result = $result"
+    Write-Host "Divver done "
+
+    trap{
+        Write-Host "Oh No!!! An error has occurred !!!"
+        Write-Host $_.ErrorID
+        Write-Host $_.Exception.Message
+        break # break with break or omitting it error bubbles up the parent.
+    }
+
+}
+
+
+Divver 33 0
+
+Clear-Host
+
+
+function Divver($enum, $denom)
+{
+    trap [System.DivideByZeroException]
+    {
+        Write-Host "Hey..... you cant divide by zero !!!"
+        continue
+    }
+
+    trap{
+        Write-Host "Oh No!!! An error has occurred !!!"
+        Write-Host $_.ErrorID
+        Write-Host $_.Exception.Message
+        break # break with break or omitting it error bubbles up the parent.
+    }
+
+    
+    Write-Host "Divver Begins"
+    $result = $enum / $denom
+    Write-Host "Result = $result"
+    Write-Host "Divver done "
+
+
+}
+
+Clear-Host
+Divver 33 0
+
+
+# Two main ways to Handle errors
+# Option 1 - Handle Error Internally - See above function
+# Option 2 - Add trap logic in parent
+
+# Change continue to break
+
+function Divver($enum, $denom)
+{
+    trap [System.DivideByZeroException]
+    {
+        Write-Host "Hey..... you cant divide by zero !!!"
+        break  # breaking or omitting to bubble up
+    }
+
+    trap{
+        Write-Host "Oh No!!! An error has occurred !!!"
+        Write-Host $_.ErrorID
+        Write-Host $_.Exception.Message
+        break # break with break or omitting it error bubbles up the parent.
+    }
+
+    
+    Write-Host "Divver Begins"
+    $result = $enum / $denom
+    Write-Host "Result = $result"
+    Write-Host "Divver done "
+}
+
+# Now call routine in a script block or other function
+
+& {
+    
+    Clear-Host
+    Divver 33 0
+
+    # Assume child has handled error, Keep going Silently
+    trap
+    {
+        continue
+    }
+}
+
+
+#---------------------------------------------------------------#
+# Working with Files VERY IMP
+#---------------------------------------------------------------#
+
+Clear-Host
+Set-Location "C:\GitRepos\PluralSight\Powershell\pluralsight-powershell_JHicks"
+Get-ChildItem "*.txt"
+
+Get-Content ".\Objective.txt"
+$a = Get-Content ".\Objective.txt"
+
+Clear-Host
+$a
+
+# Looks are deceptive, this is actually an array
+$a[0]
+$a[4]
+
+$a.GetType()
+
+Clear-Host
+for($i=0; $i -le $a.Count; $i++)
+{
+    $a[$i]
+}
+
+# To combine, we can use Join, passing in the seperator and the variable
+$seperator = [System.Environment]::NewLine # could have used `r`n
+$all = [string]::Join($seperator, $a)
+
+$all
+$all.GetType()
+
+Clear-Host
+
+# supports wildcards
+Set-Location "C:\GitRepos\PluralSight\Powershell\pluralsight-powershell_JHicks"
+$courses = Get-Content "*.txt"
+$courses
+
+$courses.GetType()
+
+$allcourses = [string]::Join($seperator, $courses)
+$allcourses.Gettype()
+
+# To write things to disk, use Set-Content
+
+# Just to prove its not there
+Get-ChildItem "All Courses.txt"
+
+Set-Content -Value $allcourses -Path "All Courses.txt"
+
+Get-ChildItem "All Courses.txt"
+
+# Set-Content is destructive REMEMBER, instead use Add-Content to append the content
+Add-Content -Value "Powershell added by add content function" -Path "All Courses.txt"
+Get-Content "All Courses.txt"
+
+# Clean up afterwards
+Remove-Item "All Courses.txt"
+
+
+
+
+
+
+
+
+
 
 
 
